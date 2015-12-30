8066b1944b5451ab11ea8e202f715788920c2159
navy1994 <lihaijun1994@gmail.com>
2015-12-30 09:21:41 +0800

    实现了短信注册

---
+++ FreeEat/SettingPassViewController.m
@@ -0,0 +1,37 @@
+//
+//  SettingPassViewController.m
+//  FreeEat
+//
+//  Created by mac- t4 on 15/12/29.
+//  Copyright (c) 2015年 上海科文麦格里实业有限公司. All rights reserved.
+//
+
+#import "SettingPassViewController.h"
+
+@interface SettingPassViewController ()
+
+@end
+
+@implementation SettingPassViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
