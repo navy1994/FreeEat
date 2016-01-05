0aa6620a48d2112683a17fee04b5d38e3eb6bc50
navy1994 <lihaijun1994@gmail.com>
2016-01-05 15:13:03 +0800

    修改了加入购物车的bug

---
+++ FreeEat/General/Views/试吃/ForetasteTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ForetasteTableViewCell.m
+//  FreeEat
+//
+//  Created by mac- t4 on 16/1/5.
+//  Copyright © 2016年 上海科文麦格里实业有限公司. All rights reserved.
+//
+
+#import "ForetasteTableViewCell.h"
+
+@implementation ForetasteTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
